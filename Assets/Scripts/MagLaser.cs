57bcb83319018543636b2af357271f4efb720b6c
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-05-18 01:04:50 +0200

    Different physics forces depending on ground type

--- Assets/Scripts/MagLaser.cs
+++ Assets/Scripts/MagLaser.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class MagLaser : MonoBehaviour
@@ -5,6 +6,18 @@ public class MagLaser : MonoBehaviour
 	[SerializeField] private float magnetLength = 10f;
 	[Tooltip("Target height to hover at, above the ground")] [SerializeField] private float targetHoverHeight = 5f;
 	[SerializeField] private float hoverForce = 5f;
+
+	[CanBeNull] private PhysicMaterial _groundMaterial;
+
+	/// <summary>
+	/// Dynamic friction of the ground the MagLaser is hitting.
+	/// </summary>
+	public float Friction => _groundMaterial != null ? _groundMaterial.dynamicFriction : 0f;
+
+	/// <summary>
+	/// Hover force multiplier of the ground the MagLaser is hitting.
+	/// </summary>
+	private float HoverForceMultiplier => _groundMaterial != null ? _groundMaterial.staticFriction : 1f;
 
 	private Rigidbody _shipRigidbody;
 	private Transform _thisTransform;
@@ -32,8 +45,14 @@ public class MagLaser : MonoBehaviour
 		{
 			Debug.DrawRay(pos, dir * result.distance, Color.green);
 			Debug.DrawRay(result.point, result.normal, Color.blue);
-			float force = (targetHoverHeight - result.distance) * hoverForce;
+			_groundMaterial = result.collider.sharedMaterial;
+
+			float force = (targetHoverHeight - result.distance) * (hoverForce * HoverForceMultiplier);
 			_shipRigidbody.AddForceAtPosition(result.normal * force, pos);
+		}
+		else
+		{
+			_groundMaterial = null;
 		}
 	}
 
--- Assets/Scripts/ShipControls.cs
+++ Assets/Scripts/ShipControls.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using UnityEngine;
 
 public class ShipControls : MonoBehaviour
@@ -7,16 +8,37 @@ public class ShipControls : MonoBehaviour
 
 	[SerializeField] private float thrust = 10f;
 	[SerializeField] private float turnSpeed = 10f;
+	[SerializeField] private float underwaterDrag = 0.5f;
+
+	[Tooltip("To know when to apply the underwater drag")] [SerializeField] private PhysicMaterial waterMaterial;
 
 	private Rigidbody _rigidbody;
+	private MagLaser[] _magLasers;
 
 	private void Awake()
 	{
 		_rigidbody = GetComponent<Rigidbody>();
+		_magLasers = GetComponentsInChildren<MagLaser>();
+		if(waterMaterial == null)
+			Debug.LogError(name + " ShipControls: waterMaterial is null!");
+	}
+
+	private void OnTriggerStay(Collider other)
+	{
+		//Underwater drag
+		if (other.sharedMaterial == waterMaterial)
+		{
+			_rigidbody.AddForce(-_rigidbody.velocity * underwaterDrag);
+		}
 	}
 
 	private void FixedUpdate()
 	{
+		//Friction
+		float friction = _magLasers.Average(magLaser => magLaser.Friction);
+		_rigidbody.AddForce(-_rigidbody.velocity * friction);
+
+		//Input
 		_rigidbody.AddForce(transform.forward * (Input.GetAxis("Vertical") * -thrust));
 		_rigidbody.AddTorque(transform.up * (Input.GetAxis("Horizontal") * turnSpeed));
 	}
