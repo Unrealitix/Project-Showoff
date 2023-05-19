f6baf639eb469bd1bea1bd0f96e83878d10c129e
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-05-19 23:36:18 +0200

    Smooth out landings

--- Assets/Scripts/MagLaser.cs
+++ Assets/Scripts/MagLaser.cs
@@ -5,9 +5,11 @@ public class MagLaser : MonoBehaviour
 {
 	[SerializeField] private float magnetLength = 10f;
 	[Tooltip("Target height to hover at, above the ground")] [SerializeField] private float targetHoverHeight = 5f;
-	[SerializeField] private float hoverForce = 5f;
+	[SerializeField] private float heightAdjustSpeed = 2f;
+	[SerializeField] private float hoverForce = 10f;
 
 	[CanBeNull] private PhysicMaterial _groundMaterial;
+	private float _currentHoverHeight;
 
 	/// <summary>
 	/// Dynamic friction of the ground the MagLaser is hitting.
@@ -34,6 +36,7 @@ public class MagLaser : MonoBehaviour
 			Debug.LogError("MagLaser must be a child of a Ship!");
 
 		_shipRigidbody = transform.parent.GetComponent<Rigidbody>();
+		_currentHoverHeight = targetHoverHeight;
 	}
 
 	private void FixedUpdate()
@@ -41,18 +44,21 @@ public class MagLaser : MonoBehaviour
 		Vector3 pos = _thisTransform.position;
 		Vector3 dir = -_thisTransform.up;
 		Debug.DrawRay(pos, dir * magnetLength, Color.red);
+		Debug.DrawRay(pos, dir * _currentHoverHeight, Color.yellow);
 		if (Physics.Raycast(pos, dir, out RaycastHit result, magnetLength))
 		{
 			Debug.DrawRay(pos, dir * result.distance, Color.green);
 			Debug.DrawRay(result.point, result.normal, Color.blue);
 			_groundMaterial = result.collider.sharedMaterial;
 
-			float force = (targetHoverHeight - result.distance) * (hoverForce * HoverForceMultiplier);
+			float force = (_currentHoverHeight - result.distance) * (hoverForce * HoverForceMultiplier);
 			_shipRigidbody.AddForceAtPosition(result.normal * force, pos);
+			_currentHoverHeight = Mathf.Lerp(_currentHoverHeight, targetHoverHeight, Time.fixedDeltaTime * 2*heightAdjustSpeed);
 		}
 		else
 		{
 			_groundMaterial = null;
+			_currentHoverHeight = Mathf.Lerp(_currentHoverHeight, magnetLength, Time.fixedDeltaTime * heightAdjustSpeed);
 		}
 	}
 
