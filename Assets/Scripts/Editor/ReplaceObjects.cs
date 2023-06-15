60df345a2ae6480233d6f3ffe4c8916389aca961
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-06-15 21:04:13 +0200

    Some minor code style cleanups

--- Assets/Scripts/Editor/ReplaceObjects.cs
+++ Assets/Scripts/Editor/ReplaceObjects.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using Random = UnityEngine.Random;
 
 
 namespace Editor
@@ -9,87 +8,84 @@ namespace Editor
 	public class ReplaceObjects : EditorWindow
 	{
 		private int _size = 1;
-		public List<GameObject> gameObjectList = new List<GameObject>();
+		public List<GameObject> gameObjectList = new();
 
 		[MenuItem("CustomTools/Replace Object")]
 		private static void Init()
 		{
-			ReplaceObjects window = (ReplaceObjects)GetWindow(typeof(ReplaceObjects), false, "Replace Object");
+			ReplaceObjects window = (ReplaceObjects) GetWindow(typeof(ReplaceObjects), false, "Replace Object");
 			window.Show();
-			
 		}
-		
+
 		private void OnGUI()
 		{
 			//Selection information display
 			GUILayout.Label("Replace selected object(s) with assets", EditorStyles.boldLabel);
 			EditorGUILayout.LabelField("Currently selected object(s): " + Selection.objects.Length);
 			GUILayout.Space(10);
-			
+
 			//List display
 			GUILayout.Label("List of replacement assets", EditorStyles.boldLabel);
 			GUILayout.Space(10);
-			
-			_size = Mathf.Max(0,EditorGUILayout.IntField("Amount", gameObjectList.Count));
-			
+
+			_size = Mathf.Max(0, EditorGUILayout.IntField("Amount", gameObjectList.Count));
+
 			while (_size > gameObjectList.Count)
 			{
 				gameObjectList.Add(null);
 			}
-			
+
 			while (_size < gameObjectList.Count)
 			{
-				gameObjectList.RemoveAt(gameObjectList.Count -1);
+				gameObjectList.RemoveAt(gameObjectList.Count - 1);
 			}
 
 			//display list items
-			for (var i = 0; i < gameObjectList.Count; i++)
+			for (int i = 0; i < gameObjectList.Count; i++)
 			{
 				GUILayout.Space(2);
 				EditorGUI.indentLevel++;
 				GUILayout.BeginHorizontal();
-				
-				gameObjectList [i] = EditorGUILayout.ObjectField("Asset " + i,gameObjectList[i], typeof(GameObject), false) 
+
+				gameObjectList[i] = EditorGUILayout.ObjectField("Asset " + i, gameObjectList[i], typeof(GameObject), false)
 					as GameObject;
-				
+
 				if (GUILayout.Button("Replace", GUILayout.MaxWidth(80)))
 					ReplaceWithPrefab(gameObjectList[i]);
-				
+
 				GUILayout.EndHorizontal();
 				EditorGUI.indentLevel--;
-				
 			}
-			
+
 			GUILayout.Space(10);
 			if (gameObjectList.Count == 0)
 				GUI.enabled = false;
 			if (GUILayout.Button("Replace with random asset from list"))
 				ReplaceWithRandom();
-			
 		}
 
 		private static void ReplaceWithPrefab(GameObject prefab)
 		{
-			var selection = Selection.gameObjects;
+			GameObject[] selection = Selection.gameObjects;
 
-			for (var i = selection.Length - 1; i >= 0; --i)
+			for (int i = selection.Length - 1; i >= 0; --i)
 			{
 				GameObject selected = selection[i];
 				PrefabAssetType prefabType = PrefabUtility.GetPrefabAssetType(prefab);
 				GameObject newObject;
-				
+
 				//spawn new object
 				if (prefabType == PrefabAssetType.Regular)
 				{
-					newObject = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+					newObject = (GameObject) PrefabUtility.InstantiatePrefab(prefab);
 				}
 				else
 				{
 					newObject = Instantiate(prefab);
 					newObject.name = prefab.name;
 				}
-				
-				
+
+
 				//Add new object to undo, copy transforms and destroy old object
 				Undo.RegisterCreatedObjectUndo(newObject, "Replace Object");
 				newObject.transform.parent = selected.transform.parent;
@@ -98,16 +94,14 @@ namespace Editor
 				newObject.transform.localScale = selected.transform.localScale;
 				newObject.transform.SetSiblingIndex(selected.transform.GetSiblingIndex());
 				Undo.DestroyObjectImmediate(selected);
-
 			}
-
 		}
 
 		private void ReplaceWithRandom()
 		{
-			var selection = Selection.gameObjects;
+			GameObject[] selection = Selection.gameObjects;
 
-			for (var i = selection.Length - 1; i >= 0; --i)
+			for (int i = selection.Length - 1; i >= 0; --i)
 			{
 				GameObject selected = selection[i];
 				GameObject newObject;
@@ -118,7 +112,7 @@ namespace Editor
 				//spawn new object
 				if (prefabType == PrefabAssetType.Regular)
 				{
-					newObject = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+					newObject = (GameObject) PrefabUtility.InstantiatePrefab(prefab);
 				}
 				else
 				{
@@ -135,7 +129,6 @@ namespace Editor
 				newObject.transform.localScale = selected.transform.localScale;
 				newObject.transform.SetSiblingIndex(selected.transform.GetSiblingIndex());
 				Undo.DestroyObjectImmediate(selected);
-
 			}
 		}
 	}
