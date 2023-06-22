66918b28a864d0ea632dbf0318d127a746475f18
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-06-22 15:17:19 +0200

    Add Checkpoints Line, and refine checkpoint pos's

--- Assets/Scripts/Checkpoints/CheckpointManager.cs
+++ Assets/Scripts/Checkpoints/CheckpointManager.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
+using NaughtyAttributes;
 using UnityEngine;
 
 namespace Checkpoints
 {
+	[ExecuteInEditMode]
 	public class CheckpointManager : MonoBehaviour
 	{
 		public static CheckpointManager Instance { get; private set; }
@@ -13,12 +15,34 @@ namespace Checkpoints
 		{
 			Instance = this;
 
+			SetupList();
+		}
+
+		[Button]
+		private void SetupList()
+		{
 			cpList = new List<Checkpoint>();
 			foreach (Transform cpTransform in transform)
 			{
 				Checkpoint cp = cpTransform.GetComponentInChildren<Checkpoint>();
-				Debug.Log(cp);
 				cpList.Add(cp);
+			}
+		}
+
+		private void OnDrawGizmos()
+		{
+			if (cpList == null) return;
+
+			//Draw line between checkpoints
+			for (int i = 0; i < cpList.Count-1; i++)
+			{
+				Checkpoint cp = cpList[i];
+				if (cp == null) continue;
+
+				Vector3 currentPos = cp.transform.position;
+				Vector3 nextPos = cpList[i + 1].transform.position;
+				Gizmos.color = Color.green;
+				Gizmos.DrawLine(currentPos, nextPos);
 			}
 		}
 	}
