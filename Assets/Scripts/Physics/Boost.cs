a10dddc3f4913b7a7736385ff69d583dc42d147c
Maike <maikekatrinski@gmail.com>
2023-06-27 11:27:27 +0200

    Add boost particles

--- Assets/Scripts/Physics/Boost.cs
+++ Assets/Scripts/Physics/Boost.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Physics
@@ -5,10 +6,19 @@ namespace Physics
 	[RequireComponent(typeof(Collider))]
 	public class Boost : MonoBehaviour
 	{
+		private ParticleSystem ps;
+		
 		private void Awake()
 		{
 			GetComponent<Collider>().isTrigger = true;
 			gameObject.layer = LayerMask.GetMask("Ignore Raycast");
+			ps = GetComponent<ParticleSystem>();
 		}
+
+		private void OnTriggerEnter()
+		{
+			ps.Play();
+		}
+		
 	}
 }
